a7961f3879a78ea20c5f5c6b667b21018dbf1e48
machinesandpixels <>
2022-09-11 23:36:25 +0200

    feat(Redux): implemented Redux Toolkit for global state management

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getStates } from '../redux/usaStatesState';
 
 const HomePage = () => {
+  const dispatch = useDispatch();
+  const states = useSelector(state => state.usaStates.states);
+
+  useEffect( () => {
+    dispatch(getStates());
+  },[dispatch]);
+
   return (
+  
     <div className=''>
       HomePage
+      { states.map( v => {
+        return <p key={v.id}> {v.title} </p>
+      }) }
     </div>
   )
 }
---
+++ src/redux/usaStatesState.js
@@ -0,0 +1,55 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const getStates = createAsyncThunk(
+  'usaStates/getStates',
+  async () => {
+  const { data } = await axios.get('https://jsonplaceholder.typicode.com/todos');
+    return data;
+ }
+);
+
+export const usaStatesSlice = createSlice({
+  name: 'usaStates',
+  initialState: {
+    states: [],
+    isLoading: false,
+  },
+  extraReducers: {
+    [getStates.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [getStates.fulfilled]: (state, action) => {
+      state.states = action.payload;
+      state.isLoading = false;
+    },
+    [getStates.rejected]: (state) => {
+      state.isLoading = false;
+    }
+  }
+});
+
+export default usaStatesSlice.reducer;
+
+// export const counterSlice = createSlice({
+//   name: "counter",
+//   initialState: {
+//     count: 0
+//   },
+//   reducers: {
+//     increment: (state) => {
+//       state.count += 1;
+//     },
+//     decrement: (state) => {
+//       state.count -= 1;
+//     },
+//     incrementByAmount: (state, action) => {
+//       state.count += action.payload;
+//     }
+//   }
+// });
+
+// // Action creators are generated for each case reducer function
+// export const { increment, decrement, incrementByAmount } = counterSlice.actions;
+
+// export default counterSlice.reducer;
