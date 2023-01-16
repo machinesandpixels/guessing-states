cff6c0d77ba9ed9dc18ebe39c7274e2aa8d27d5a
Machines & Pixels <quezada.dev@gmail.com>
2023-01-16 18:45:01 -0500

    feat(StatesContainer): created StatesContainer component

---
+++ src/components/StatesContainer.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getStates } from '../redux/usaStatesState';
+import styled from 'styled-components';
+
+const StyledDiv = styled.div`
+    display: flex;
+    justify-content: space-between;
+    max-width: 700px;
+    background-color: black;
+    color: antiquewhite;
+    margin: 0 auto;
+    padding: 2rem;
+`;
+
+const StyledParagraph = styled.p`
+    border-left: solid white 3px;
+    border-right: solid white 3px;
+    padding: .5rem;
+`;
+
+const StatesContainer = () => {
+    const dispatch = useDispatch();
+    const states = useSelector(state => state.usaStates.states);
+
+    useEffect( () => {
+        dispatch(getStates());
+    },[dispatch]);
+
+  return (
+    <StyledDiv>
+        { states.map( v => {
+            return <StyledParagraph key={v.id}> {v.title} </StyledParagraph>
+        }) }
+    </StyledDiv>
+  )
+}
+
+export default StatesContainer;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,24 +1,13 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { getStates } from '../redux/usaStatesState';
+import React from 'react';
+import StatesContainer from '../components/StatesContainer';
 
 const HomePage = () => {
-  const dispatch = useDispatch();
-  const states = useSelector(state => state.usaStates.states);
-
-  useEffect( () => {
-    dispatch(getStates());
-  },[dispatch]);
-
-  return (
   
-    <div className=''>
-      HomePage
-      { states.map( v => {
-        return <p key={v.id}> {v.title} </p>
-      }) }
+  return (
+    <div className='home'>
+     <StatesContainer />
     </div>
-  )
+  );
 }
 
 export default HomePage;
