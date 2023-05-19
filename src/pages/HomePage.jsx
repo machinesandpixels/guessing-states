3727dad7759f8e40cfd2ca9e7808d5a745e7e2f4
machinesandpixels <quezada.dev@gmail.com>
2023-05-18 23:26:22 -0700

    feat(HomePage): added image to HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import styled from 'styled-components';
+import uncleSam from '../images/uncle_sam.jpeg';
 
+const StyledContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    margin: 0 auto;
+`;
+
+const StyledImg = styled.img`
+    width: 300px;
+    height: 300px;
+`;
 
 const HomePage = () => {
   return (
-    <div className='home'>
-    </div>
+    <StyledContainer>
+      <StyledImg src={uncleSam} alt="Uncle Sam" srcset="" />
+    </StyledContainer>
   );
 }
 
