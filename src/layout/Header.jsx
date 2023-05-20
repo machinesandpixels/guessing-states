5b53c75787fe0636e7be548eef9a784154d6ffa9
machinesandpixels <quezada.dev@gmail.com>
2023-05-19 18:35:04 -0700

    feat(HomePage): centered image, header and added lets play button

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -4,9 +4,11 @@ import styled from 'styled-components';
 const StyledH1 = styled.h1`
   cursor: pointer;
   color: black;
+  font-size: 2.5rem;
   margin-top: 3rem;
   text-align: center;
   transition: 0.5s all ease-out;
+  letter-spacing: .2rem;
 
   &:hover {
     color: #777777;
